880bde3e845db68c5c15108322f0638ae33cd938
Thomas Viot <viotthomas7@gmail.com>
2022-11-02 14:09:46 +0100

    Description in the home page

---
+++ src/components/Description.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import ProfileImage from "../assets/moi.jpg"
+import "../styles/DescriptionStyle.css"
+import {Link} from "react-router-dom";
+
+const Description = () => {
+  return (
+    <div className='description'>
+
+        <div className='text-zone'>
+            <div className='text-zone-wrapper'>
+              <h2 className='intro'>Hello, My name is</h2>
+              <h1 className='name'>Thomas Viot</h1>
+
+              <div className='title'>
+                <div className='title-wrapper'>
+                  <div className='title-item'>Student at ENSEIRB-MATMECA</div>
+                  <div className='title-item'>Front-end Developer</div>
+                  <div className='title-item'>Video Maker</div>
+                  <div className='title-item'>Athlete</div>
+                </div>
+              </div>
+
+              <div className='button-container'>
+                <Link to="/react-github-portfolio/Contact" className='button'>Hire me</Link>
+              </div>
+              
+            </div>
+        </div>
+
+        <div className='pictures'>
+            <img src={ProfileImage} alt="profile" className='firstPlanImage'></img>
+        </div>
+          
+        
+        
+    </div>
+  )
+}
+
+export default Description
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Description from '../components/Description'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Description />
     </div>
   )
 }
